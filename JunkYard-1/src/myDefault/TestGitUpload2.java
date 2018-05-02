2d1639fe44e7a90d1eadfb0fdaafd5689e0ffb41
Sawan.Patwari <sawan.patwari@yahoo.in>
2018-05-03 05:16:21 +0530

    Testing Git Upload-2

---
+++ JunkYard-1/src/myDefault/TestGitUpload2.java
@@ -0,0 +1,10 @@
+package myDefault;
+
+public class TestGitUpload2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git Upload-2.");
+	}
+
+}
